4220cc0b923b2bc8cdaeaa33ac01f8f83eea1625
suzhou <withwind318@gmail.com>
2024-03-01 17:27:37 +0800

    misc

--- binary-tree/binary-tree-paths-257.ts
+++ binary-tree/binary-tree-paths-257.ts
@@ -1,16 +1,25 @@
 import path from "path"
-import { TreeNode } from "./TreeNode"
+import { TreeNode, testTree } from "./TreeNode"
 
-function binaryTreePath(node: TreeNode | null, path: string): string | null {
+function binaryTreePath(node: TreeNode | null, path: string, paths: string[]) {
     if (!node) return null
 
-
-    binaryTreePath(node.left, path)
-    binaryTreePath(node.right, path)
-
-    return path + '->' + node.val
+    let p = path + '->' + node.val
+    if (!node.left && !node.right) paths.push(p)
+    binaryTreePath(node.left, path, paths)
+    binaryTreePath(node.right, path, paths)
 }
 
 function binaryTreePaths(root: TreeNode | null): string[] {
 
+    let paths: string[] = []
+    binaryTreePath(root, root?.val + '', paths)
+
+    return paths
+}
+
+
+{
+    let res = binaryTreePaths(testTree)
+    console.log(res)
 }
