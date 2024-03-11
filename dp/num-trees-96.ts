103c3a51022ed3ee384c45d8a514410666de2918
suzhou <withwind318@gmail.com>
2024-03-11 09:41:06 +0800

    dp +1

--- dp/num-trees-96.ts
+++ dp/num-trees-96.ts
@@ -1,3 +1,23 @@
+/**
+ * 利用二叉搜索树特性
+ * dp[n] += dp[i-1] * dp[n-i]
+ * @param n 
+ * @returns 
+ */
 function numTrees(n: number): number {
+    let dp = new Array(n + 1).fill(0)
+    dp[0] = 1
 
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j]
+        }
+    }
+
+    return dp[n]
+}
+
+
+{
+    console.log(numTrees(4))
 }
