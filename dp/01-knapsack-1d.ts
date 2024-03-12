a0d36b56d53928662dfbfcf60c006e00ec68b3d6
suzhou <withwind318@gmail.com>
2024-03-12 11:24:47 +0800

    01-knapsack +1

--- dp/01-knapsack-1d.ts
+++ dp/01-knapsack-1d.ts
@@ -7,28 +7,15 @@
  * dp[cap] - 容量为cap的背包所背的最大价值
  * 
  * 递推公式：
- * dp[cap] = max(dp[cap-1], dp[cap-weight[i]] + value[i])
- * 
- * 如果物品i的重量大于cap，无法放入背包，则：
- * dp[cap] = dp[cap-1]
- * 反之，则：
- * dp[cap] = dp[cap-weight[i]] + value[i]
+ * dp[cap] = max(dp[cap], dp[cap-weight[i]] + value[i])
  */
 function maxValue1d(weight: number[], value: number[], w: number): number {
     let num = weight.length
-
     let dp: number[] = new Array(w + 1).fill(0)
-    for (let itemIndex = 0; itemIndex < num; itemIndex++) {
-        if (weight[itemIndex] <= 1) dp[1] = Math.max(value[itemIndex], dp[1])
-    }
 
-    for (let cap = 1; cap <= w; cap++) {
-        for (let i = 0; i < num; i++) {
-            if (weight[i] > cap) {
-                dp[cap] = dp[cap - 1]
-            } else {
-                dp[cap] = Math.max(dp[cap - 1], dp[cap - weight[i]] + value[i])
-            }
+    for (let i = 0; i < num; i++) {
+        for (let cap = w; cap >= weight[i]; cap--) {
+            dp[cap] = Math.max(dp[cap], dp[cap - weight[i]] + value[i])
         }
     }
 
@@ -36,8 +23,17 @@ function maxValue1d(weight: number[], value: number[], w: number): number {
 }
 
 {
-    let weight = [1, 3, 4]
-    let value = [15, 20, 30]
-    let res = maxValue(weight, value, 4)
+    let weight = [1, 2, 3, 4]
+    let value = [15, 20, 25, 30]
+    let res = maxValue1d(weight, value, 7)
     console.log(res)
 }
+
+/**
+ * (0)5      (0)4      (0)3      (0)2   (0)1
+ * (15)5     (15)4     (15)3     (15)2  (15)1
+ * (15+20)5  (15+20)4  (15+20)3  (20)2  (15)1
+ * (25+20)5  (25+15)4  (15+20)3  (20)2  (15)1
+ * (25+20)5  (25+15)4  (15+20)3  (20)2  (15)1
+ */
+
