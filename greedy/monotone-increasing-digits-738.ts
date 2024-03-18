32b1c508a6e26e889ec6974ab0b572035f07f095
suzhou <withwind318@gmail.com>
2024-03-18 17:08:11 +0800

    misc

---
+++ greedy/monotone-increasing-digits-738.ts
@@ -0,0 +1,18 @@
+function monotoneIncreasingDigits(n: number): number {
+    let first = n.toString[0]
+    let min = ''
+    for (let i = 0; i < n.toString.length; i++) {
+        min += first
+    }
+    if (n < parseInt(min)) {
+
+    }
+
+
+    return first
+}
+
+
+{
+    console.log(monotoneIncreasingDigits(8098776679))
+}
