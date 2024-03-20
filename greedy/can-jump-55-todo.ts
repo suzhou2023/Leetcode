2ceadbc44e56eae461779947e821b46a0b8175c4
suzhou <withwind318@gmail.com>
2024-03-20 14:14:57 +0800

    greedy +1

--- greedy/can-jump-55-todo.ts
+++ greedy/can-jump-55-todo.ts
@@ -1,11 +1,16 @@
-function canJump(nums: number[]): boolean {
-    let next = 0, cover = 0
-    while (next <= cover) {
-        if (next + nums[next] > cover) {
-            cover = next + nums[next]
-        }
+/**
+ * 在当前位置的覆盖范围，找下一步的最大覆盖范围
+ * 简单两行代码，还真不好想通
+ * 
+ */
 
-        next++
+function canJump(nums: number[]): boolean {
+    if (nums.length == 1) return true
+
+    let cover = 0
+    for (let i = 0; i <= cover; i++) {
+        cover = Math.max(i + nums[i], cover)
+        if (cover >= nums.length - 1) return true
     }
 
     return false
