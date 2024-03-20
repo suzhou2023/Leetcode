2b67d8938bd086afd00e1c37f447c481b7d01b16
suzhou <withwind318@gmail.com>
2024-03-20 11:09:23 +0800

    greedy +1

---
+++ greedy/max-profit-122.ts
@@ -0,0 +1,24 @@
+function maxProfit(prices: number[]): number {
+
+    let i = 0, profit = 0, buy = Number.MIN_SAFE_INTEGER
+    while (i < prices.length) {
+        if (i == prices.length - 1 && buy > Number.MIN_SAFE_INTEGER) profit += prices[i] - buy
+
+        if (prices[i + 1] > prices[i] && buy == Number.MIN_SAFE_INTEGER) {
+            buy = prices[i]
+        } else if (prices[i + 1] < prices[i] && buy > Number.MIN_SAFE_INTEGER) {
+            profit += prices[i] - buy
+            buy = Number.MIN_SAFE_INTEGER
+        }
+
+        i++
+    }
+
+    return profit
+}
+
+{
+    // let prices = [2, 1, 2, 0, 1]
+    // let res = maxProfit(prices)
+    // console.log(res)
+}
